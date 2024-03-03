c885be9060d2eacadf4ee907a488a2f17921da8a
Sreenivasulu <57563260+Sreenivasulug55@users.noreply.github.com>
2024-03-04 00:10:09 +0530

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,11 +2,23 @@
 public abstract class Player {
 	String name;
 	char marker;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getMarker() {
+		return marker;
+	}
+	public void setMarker(char marker) {
+		this.marker = marker;
+	}
 	abstract void makemove();
 	boolean isValidMove(int row, int col) {
 		boolean result = false;
 		if (row >= 0 && row <= 2 && col >= 0 && col <= 2) {
-			if (TicTacToe.Board[row][col] == ' ') {
+			if (TicTacToe.board[row][col] == ' ') {
 				result = true;
 			} else
 				result = false;
